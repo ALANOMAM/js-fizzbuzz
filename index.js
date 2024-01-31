b8eb961d6456f588c439c968dda138b2bcc78cde
alan_omam <omamalan@hotmail.it>
2024-01-31 15:46:13 +0100

    sisyemato la parte della console con fizz, buzz e fizzbuzz applicati

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
 
 
 for(let i=1;i<=100;i++){
-    console.log(i)
+    
+    
+    if(i % 3 == 0 && !(i % 5 == 0)){
+        console.log("fizz",i)
+     }else if(i % 5 == 0 && !(i % 3 == 0)){
+        console.log("buzz",i)
+
+     } else if(i % 3 == 0 && i % 5 == 0){
+        console.log("fizzbuz",i)
+     } else{
+        console.log(i)
+     }
+
+         
 }
+
