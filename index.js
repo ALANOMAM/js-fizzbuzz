299f7e71d8cff8bd071a0d9ad348007a3116c082
alan_omam <omamalan@hotmail.it>
2024-01-31 17:10:38 +0100

    FINE PROGRAMMA(aggiunto commneti, messo un pò di ordine al programma e aggiunto titolo)

--- index.js
+++ index.js
@@ -2,37 +2,34 @@
 let grigliaElementi = document.querySelector("#block")
 
 for(let i=1;i<=100;i++){
-    
+    //creo i div che mi serviranno per contenere i contenuti, da appendere dopo al block sopra
     const Element = document.createElement("div")
 
-     
-      Element.className="square";
+     //qui aggiungo una classe e sistemo lo stile del mio elemento appena creato
+     Element.className="square";
      Element.style.color="black";
      Element.style.fontSize="20px";
 
      
 
-
-
     if(i % 3 == 0 && !(i % 5 == 0)){
-        console.log("fizz",i)
-        Element.innerHTML="Fizz"
+    //cambio i contenuti e aggiungo un'altra classe ai mie div creati sopra a seconda del valore di i
+        Element.innerHTML="Fizz" 
+    //occhio a LASCIARE LO SPAZIO A SINISTRA quando si aggiunge una seconda classe     
         Element.className=" fizzSquare"
      }else if(i % 5 == 0 && !(i % 3 == 0)){
-        console.log("buzz",i)
         Element.innerHTML="Buzz"
         Element.className=" buzzSquare"
 
-     } else if(i % 3 == 0 && i % 5 == 0){
-        console.log("fizzbuz",i)
+     } else if(i % 3 == 0 && i % 5 == 0){  
         Element.innerHTML="FizzBuzz"
         Element.className=" fizzBuzzSquare"
      } else{
-        console.log(i)
         Element.innerHTML=i
      }
 
-
+  // qui appendo i miei div creati sopra al div contenitore con id "block" che ho chiamato "grigliaElementi"
+  // andranno all'interno del mio contenitore block.
      grigliaElementi.append(Element)
       
 }
