64ed737cc9055e40397a7c7cf9cbccdedd395d2c
alan_omam <omamalan@hotmail.it>
2024-01-31 16:06:55 +0100

    impostato i 100 numeri con fizz, buzz et fizzbuzz in dom. Senza stile per ora.

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 
-
+let grigliaElementi = document.querySelector("#griglia")
 
 for(let i=1;i<=100;i++){
     
-    
+    const Element = document.createElement("div")
+     
+     /*Element.innerHTML=i;
+      Element.className="square";
+     Element.style.color="red";*/
+
+     
+
+
+
+    if(i % 3 == 0 && !(i % 5 == 0)){
+        console.log("fizz",i)
+        Element.innerHTML="Fizz"
+     }else if(i % 5 == 0 && !(i % 3 == 0)){
+        console.log("buzz",i)
+        Element.innerHTML="Buzz"
+
+     } else if(i % 3 == 0 && i % 5 == 0){
+        console.log("fizzbuz",i)
+        Element.innerHTML="FizzBuzz"
+     } else{
+        console.log(i)
+        Element.innerHTML=i
+     }
+
+
+     grigliaElementi.append(Element)
+
+    /*
     if(i % 3 == 0 && !(i % 5 == 0)){
         console.log("fizz",i)
      }else if(i % 5 == 0 && !(i % 3 == 0)){
@@ -13,7 +41,7 @@ for(let i=1;i<=100;i++){
         console.log("fizzbuz",i)
      } else{
         console.log(i)
-     }
+     }*/
 
          
 }
